1a8a7da69955479c04ad75a2d4bcb3eacc5efa1e
Tarek Eldeeb <tarekeldeeb@msn.com>
2018-06-05 13:49:02 +0200

    Use the arabic corpus in demo. No evaluation yet.

--- demo.sh
+++ demo.sh
@@ -5,17 +5,17 @@ set -e
 # One optional argument can specify the language used for eval script: matlab, octave or [default] python
 
 make
-if [ ! -e text8 ]; then
-  if hash wget 2>/dev/null; then
-    wget http://mattmahoney.net/dc/text8.zip
-  else
-    curl -O http://mattmahoney.net/dc/text8.zip
-  fi
-  unzip text8.zip
-  rm text8.zip
-fi
+#if [ ! -e text8 ]; then
+#  if hash wget 2>/dev/null; then
+#    wget http://mattmahoney.net/dc/text8.zip
+#  else
+#    curl -O http://mattmahoney.net/dc/text8.zip
+#  fi
+#  unzip text8.zip
+#  rm text8.zip
+#fi
 
-CORPUS=text8
+CORPUS=arabic.cp1256
 VOCAB_FILE=vocab.txt
 COOCCURRENCE_FILE=cooccurrence.bin
 COOCCURRENCE_SHUF_FILE=cooccurrence.shuf.bin
